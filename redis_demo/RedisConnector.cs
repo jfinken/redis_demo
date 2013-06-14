19bdb5dfcfda315f8d206a6ff41a8c3570a356b7
Josh Finken <josh.finken@nokia.com>
2013-06-14 12:22:31 -0600

    initial commit on hacking redis pub\sub with c# sockets and the redis wire protocol.

---
+++ redis_demo/RedisConnector.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using System.Diagnostics;
+
+namespace redis_demo 
+{
+    class RedisConnector
+    {
+        private int _port;
+        private string _ipAddress;
+        private string _name;
+        //private string _authHash;
+
+        private Socket _generalSocket; //for SET, GET, PUBLISH
+        //private Socket _subscribeSocket;  //for SUBSCRIBE
+
+        public RedisConnector()
+        {
+        
+        }
+        // JF
+        public String Name {
+            get {return _name;}
+            set {_name = value;}
+        }
+        public void connect(string ipAddress, int port)
+        {
+            _port = port;
+            _ipAddress = ipAddress;
+            
+            // create the SET GET PUBLISH socket 
+            _generalSocket = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            // create endpoint 
+            var ipAdd = IPAddress.Parse(_ipAddress);
+            var endpoint = new IPEndPoint(ipAdd, _port);
+
+            // create event args 
+            var args = new SocketAsyncEventArgs();
+            args.RemoteEndPoint = endpoint;
+            args.Completed += GeneralSocketConnected;
+            //args.Completed += new EventHandler<SocketAsyncEventArgs>(SocketEventArg_Completed);
+            args.UserToken = _generalSocket;    //jf
+
+            // check if the completed event will be raised. If not, invoke the handler manually. 
+            if (!_generalSocket.ConnectAsync(args))
+                GeneralSocketConnected(args.ConnectSocket, args);
+        }
+        /// <summary>
+        /// A single callback is used for all socket operations. This method forwards execution on to the correct handler 
+        /// based on the type of completed operation
+        /// </summary>
+        static void SocketEventArg_Completed(object sender, SocketAsyncEventArgs e)
+        {
+            switch (e.LastOperation)
+            {
+                case SocketAsyncOperation.Connect:
+                    //ProcessConnect(e);
+                    break;
+                case SocketAsyncOperation.Receive:
+                    //ProcessReceive(e);
+                    break;
+                case SocketAsyncOperation.Send:
+                    //ProcessSend(e);
+                    break;
+                default:
+                    throw new Exception("Invalid operation completed");
+            }
+        }
+        private void GeneralSocketConnected(object sender, SocketAsyncEventArgs e)
+        {
+            // check for errors 
+            if (e.SocketError != System.Net.Sockets.SocketError.Success)
+            {
+                Debug.WriteLine("ERROR CONNECTING WITH GENERAL SOCKET WITH SERVER");
+                return;
+            }
+            else
+            {
+               // Debug.WriteLine("GENERAL SOCKET CONNECTED WITH SERVER");
+
+                if (m_connected != null)
+                {
+                    m_connected(this, new RedisEventArgs("connected"));
+                }
+
+            }
+            e.Completed -= GeneralSocketConnected;
+
+            listenAndReceive();
+        }
+        private void listenAndReceive() {
+            //-----------------------------------------------------------------
+            //jf
+            // 
+            // Know that this instance of a RedisConnector
+            //-----------------------------------------------------------------
+            if(_name == "im_the_subscriber_bitch") {
+                SocketAsyncEventArgs receiveSocketEventArg = new SocketAsyncEventArgs();
+                receiveSocketEventArg.RemoteEndPoint = _generalSocket.RemoteEndPoint;
+                // Setup the buffer to receive the data
+                receiveSocketEventArg.SetBuffer(new byte[512], 0, 512);
+                receiveSocketEventArg.Completed += new EventHandler<SocketAsyncEventArgs>(onGeneralSocketMessageIn);
+
+                if (!_generalSocket.ReceiveAsync(receiveSocketEventArg))
+                    onGeneralSocketMessageIn(this, receiveSocketEventArg);
+
+            }
+        }
+
+        void onGeneralSocketMessageIn(object sender, SocketAsyncEventArgs e)
+        {
+            if(_name == "im_the_subscriber_bitch") {
+                Debug.WriteLine("onGenerateSocketMessageIn: I'm the subscriber...");
+            }
+
+	            if (e.BytesTransferred > e.Count)
+                    new Exception("ERROR onGeneralSocketMessageIn");
+
+                e.Completed -= onGeneralSocketMessageIn;
+
+                string response = Encoding.UTF8.GetString(e.Buffer, e.Offset, e.BytesTransferred);
+                response = response.Trim('\0');               
+
+                if (m_messageReceived != null)
+                {
+                    m_messageReceived(this, new RedisEventArgs(response));
+                }
+            }
+
+
+
+        public void sendCommand(string[] commandArgs)
+        {           
+                SocketAsyncEventArgs receiveSocketEventArg = new SocketAsyncEventArgs();
+                receiveSocketEventArg.RemoteEndPoint = _generalSocket.RemoteEndPoint;
+                // Setup the buffer to receive the data
+                receiveSocketEventArg.SetBuffer(new byte[512], 0, 512);
+                receiveSocketEventArg.Completed += new EventHandler<SocketAsyncEventArgs>(onGeneralSocketMessageIn);
+
+                if (!_generalSocket.ReceiveAsync(receiveSocketEventArg))
+                    onGeneralSocketMessageIn(this, receiveSocketEventArg);
+
+                string bufferStr = "";
+                bufferStr += "*" + commandArgs.Length.ToString() + "\r\n";
+
+                for (int i = 0; i < commandArgs.Length; i++)
+                {
+                    string str = commandArgs[i];
+                    bufferStr += "$" + str.Length.ToString() + "\r\n";
+                    bufferStr += str + "\r\n";
+                }
+                Debug.WriteLine("SENDING : " + bufferStr);
+
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(bufferStr);
+
+                SocketAsyncEventArgs sendSocketEventArg = new SocketAsyncEventArgs();
+               
+                sendSocketEventArg.SetBuffer(buffer, 0, buffer.Length);
+
+
+                bool completedAsync = false;
+
+                try
+                {
+                    completedAsync = _generalSocket.SendAsync(sendSocketEventArg);
+                }
+                catch (SocketException se)
+                {
+                    Debug.WriteLine("Socket Exception: " + se.ErrorCode + " Message: " + se.Message);
+                }
+        }
+
+        public void CleanUp()
+        {
+           _generalSocket.Shutdown(SocketShutdown.Both);
+            _generalSocket.Close();
+        }
+
+
+
+        private EventHandler<RedisEventArgs> m_connected;
+
+        public event EventHandler<RedisEventArgs> onConnected
+        {
+            add { m_connected += value; }
+            remove { m_connected -= value; }
+        }
+
+        private EventHandler<RedisEventArgs> m_messageReceived;
+
+        public event EventHandler<RedisEventArgs> onMessageReceived
+        {
+            add { m_messageReceived += value; }
+            remove { m_messageReceived -= value; }
+        }
+
+    }
+
+    public class RedisEventArgs : EventArgs
+    {
+        private string msg;
+
+        public RedisEventArgs(string messageData)
+        {
+            msg = messageData;
+        }
+        public string Message
+        {
+            get { return msg; }
+            set { msg = value; }
+        }
+    }
+
+}
---
+++ redis_demo/RedisManager.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace redis_demo 
+{
+    class RedisManager
+    {
+        private static RedisManager _instance;
+
+        private RedisConnector _redisPublisher;
+
+        private RedisConnector _redisSubscriber;
+
+        public static RedisManager instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new RedisManager();
+                }
+                return _instance;
+            }
+        }
+
+        public void start()
+        {
+            _redisPublisher = new RedisConnector();
+            _redisPublisher.onConnected += _redisPublisher_onConnected;
+            _redisPublisher.onMessageReceived += _redisPublisher_onMessageReceived;
+            _redisPublisher.connect("54.235.99.151", 24400);
+
+            _redisSubscriber = new RedisConnector();
+            _redisSubscriber.Name = "im_the_subscriber_bitch";
+            _redisSubscriber.onConnected += _redisSubscriber_onConnected;
+            _redisSubscriber.onMessageReceived += _redisSubscriber_onMessageReceived;
+            _redisSubscriber.connect("54.235.99.151", 24400);
+        }
+
+        private void _redisPublisher_onConnected(object sender, RedisEventArgs e)
+        {
+            Debug.WriteLine("_redisPublisher CONNECTED = " + e.Message);
+            _redisPublisher.sendCommand(new string[] { "AUTH", "IR1rq9w3NUIxMy1aw5EWF90WxKKcmF3g2vpdOIPLlCOwMuMeFu+3YB9N7P9ni31vjIvZA5YHcEW3" });
+
+        }
+
+        private void _redisSubscriber_onConnected(object sender, RedisEventArgs e)
+        {
+            Debug.WriteLine("_redisSubscriber CONNECTED = " + e.Message);
+            _redisSubscriber.sendCommand(new string[] { "AUTH", "IR1rq9w3NUIxMy1aw5EWF90WxKKcmF3g2vpdOIPLlCOwMuMeFu+3YB9N7P9ni31vjIvZA5YHcEW3" });
+            _redisSubscriber.sendCommand(new string[] { "SUBSCRIBE", "vehicle_status_channel" });
+        }
+
+        private void _redisSubscriber_onMessageReceived(object sender, RedisEventArgs e)
+        {
+            Debug.WriteLine("SUBSCRIBER RECEIVED = " + e.Message);
+        }
+
+        private void _redisPublisher_onMessageReceived(object sender, RedisEventArgs e)
+        {
+            Debug.WriteLine("PUBLISHER RECEIVED = " + e.Message);
+        }
+
+        public void sendCommute()
+        {
+            Random rnd1 = new Random();
+            int maVar = rnd1.Next(100);
+            _redisPublisher.sendCommand(new string[] { "PUBLISH", "vehicle_status_channel", "status_" + maVar.ToString() });
+        }
+
+        public void sendDowntime()
+        {
+            Random rnd1 = new Random();
+            int maVar = rnd1.Next(100);
+            _redisPublisher.sendCommand(new string[] { "PUBLISH", "vehicle_status_channel", "down!" + maVar.ToString() });
+        }
+
+        public void sendSet()
+        {
+            Random rnd1 = new Random();
+            int maVar = rnd1.Next(100);
+            _redisPublisher.sendCommand(new string[] { "SET", "josh", "finken_" + maVar.ToString() });
+
+        }
+
+        public void sendGet()
+        {            
+            _redisPublisher.sendCommand(new string[] { "GET", "josh" });
+        }
+
+        public void closeConnection()
+        {
+            _redisSubscriber.sendCommand(new string[] { "UNSUBSCRIBE", "vehicle_status_channel" });
+            _redisPublisher.CleanUp();
+            _redisSubscriber.CleanUp();
+        }
+
+
+    }
+}
